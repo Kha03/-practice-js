010cd880d5c806f9dd5f198137c08ddc46467469
Kha <minhkha20031996@gmail.com>
2024-08-30 23:04:21 +0700

    finish ex 1 working with array

--- Data_Structures_Modern_Operators_Strings/index.js
+++ Data_Structures_Modern_Operators_Strings/index.js
@@ -187,3 +187,43 @@ for (const [minute, event] of gameEvents) {
   const half = minute <= 45 ? "FIRST HALF" : "SECOND HALF";
   console.log(`[${half}] ${minute}: ${event}`);
 }
+//Ex 4
+// Write a program that receives a list of variable names written in underscore_case
+// and convert them to camelCase.
+// The input will come from a textarea inserted into the DOM (see code below to
+// insert the elements), and conversion will happen when the button is pressed.
+// Test data (pasted to textarea, including spaces):
+// underscore_case
+// first_name
+// Some_Variable
+//  calculate_AGE
+// delayed_departure
+// Should produce this output (5 separate console.log outputs):
+// underscoreCase ✅
+// firstName ✅✅
+// someVariable ✅✅✅
+// calculateAge ✅✅✅✅
+// delayedDeparture ✅✅✅✅✅
+// Hints:
+// § Remember which character defines a new line in the textarea �
+// § The solution only needs to work for a variable made out of 2 words, like a_b
+// § Start without worrying about the ✅. Tackle that only after you have the variable
+// name conversion working �
+// § This challenge is difficult on purpose, so start watching the solution in case
+// you're stuck. Then pause and continue!
+const text = document.querySelector("#convert");
+const btn = document.querySelector(".submit");
+btn.onclick = () => {
+  const textArr = text.value.split("\n");
+  textArr.forEach((str, i) => {
+    const word = str.trim().split("_");
+    word.length === 1
+      ? console.log(`${word[0].toLowerCase()}`)
+      : console.log(
+          `${word[0].toLowerCase()}${word
+            .slice(1)
+            .map((word) => word.replace(word[0], word[0].toUpperCase()))
+            .join("")} ${"✅".repeat(i + 1)}`
+        );
+  });
+};
