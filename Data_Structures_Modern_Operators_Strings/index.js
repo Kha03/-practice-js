7aa3de8dafec4b99b928efe238032b2d95280d46
Kha <minhkha20031996@gmail.com>
2024-08-30 10:39:26 +0700

    finish Challenge 3 Data_Structures

--- Data_Structures_Modern_Operators_Strings/index.js
+++ Data_Structures_Modern_Operators_Strings/index.js
@@ -103,3 +103,87 @@ team1 > team2 && console.log("Team 2 is more likely to win");
 // Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
 printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
 // Then, call the function again with players from game.scored
+// Let's continue with our football betting app! Keep using the 'game' variable from
+// before.
+// Your tasks:
+// 1. Loop over the game.scored array and print each player name to the console,
+// along with the goal number (Example: "Goal 1: Lewandowski")
+game.scored.forEach((player, i) => console.log(`Goal ${i + 1}: ${player}`));
+// 2. Use a loop to calculate the average odd and log it to the console (We already
+// studied how to calculate averages, you can go check if you don't remember)
+const oddAverage =
+  Object.values(game.odds).reduce((acc, odd) => acc + odd, 0) /
+  Object.values(game.odds).length;
+console.log(oddAverage);
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them
+// (except for "draw"). Hint: Note how the odds and the game objects have the
+// same property names �
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr}: ${odd}`);
+}
+// 4. Bonus: Create an object called 'scorers' which contains the names of the
+// players who scored as properties, and the number of goals as the value. In this
+// game, it will look like this:
+// {
+//  Gnarby: 1,
+//  Hummels: 1,
+//  Lewandowski: 2
+// }
+const scorers = {};
+game.scored.forEach((player) => {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+});
+console.log(scorers);
+// Let's continue with our football betting app! This time, we have a map called
+// 'gameEvents' (see below) with a log of the events that happened during the
+// game. The values are the events themselves, and the keys are the minutes in which
+// each event happened (a football game has 90 minutes plus some extra time).
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "� Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "� Substitution"],
+  [64, "� Yellow card"],
+  [69, "� Red card"],
+  [70, "� Substitution"],
+  [72, "� Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "� Yellow card"],
+]);
+// Your tasks:
+// 1. Create an array 'events' of the different game events that happened (no
+// duplicates)
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+// 2. After the game has finished, is was found that the yellow card from minute 64
+// was unfair. So remove this event from the game events log.
+gameEvents.delete(64);
+console.log(gameEvents);
+// 3. Compute and log the following string to the console: "An event happened, on
+// average, every 9 minutes" (keep in mind that a game has 90 minutes)
+const eventHappend = {};
+gameEvents.forEach((value, key) => {
+  eventHappend[value]
+    ? (eventHappend[value][1]++,
+      (eventHappend[value][0] = key - eventHappend[value][0]))
+    : (eventHappend[value] = [key, 1]);
+});
+console.log(eventHappend);
+for (const event in eventHappend) {
+  const [totalTime, count] = eventHappend[event];
+  averages = totalTime / count;
+  console.log(`An ${event} happened, on average, every ${averages} minutes`);
+}
+// 4. Loop over 'gameEvents' and log each element to the console, marking
+// whether it's in the first half or second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17: ⚽ GOAL
+for (const [minute, event] of gameEvents) {
+  const half = minute <= 45 ? "FIRST HALF" : "SECOND HALF";
+  console.log(`[${half}] ${minute}: ${event}`);
+}
